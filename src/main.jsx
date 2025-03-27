5d6baadf2a226d4cd037bfd79f2ddef52098cebc
Jialegoh <cteh60112@gmail.com>
2025-03-27 14:46:43 +0800

    push

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* Replace BrowserRouter with HashRouter */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
